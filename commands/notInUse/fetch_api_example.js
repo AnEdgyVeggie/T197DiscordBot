808a0b72015249a9afddb99c3525a724064585f9
nk-tran <nktran4@gmail.com>
2023-09-30 02:23:00 -0400

    decommissioned testing functions

--- commands/notInUse/fetch_api_example.js
+++ commands/notInUse/fetch_api_example.js
@@ -1,18 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('not_in_use')
-        .setDescription('See upcoming due dates'),
-    async execute(interaction) {
-        fetch('https://codeshare.io/AdwlMN')
-            .then(res => res.text())
-            .then(text => {
-                interaction.reply(text);
-            })
-            .catch(err => {
-                console.error(err);
-                interaction.reply('Error fetching text');
-            });
-    }
-}
+// const { SlashCommandBuilder } = require('discord.js');
+//
+// module.exports = {
+//     data: new SlashCommandBuilder()
+//         .setName('not_in_use')
+//         .setDescription('See upcoming due dates'),
+//     async execute(interaction) {
+//         fetch('https://codeshare.io/AdwlMN')
+//             .then(res => res.text())
+//             .then(text => {
+//                 interaction.reply(text);
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//                 interaction.reply('Error fetching text');
+//             });
+//     }
+// }
